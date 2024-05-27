f3fc486236c7d753fad98a624136268cfd04ee9a
Michal Fasanek <mfasanek@launchdarkly.com>
2024-05-27 15:45:38 +0200

    ld pt1

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+// Below are the custom imports
+import { asyncWithLDProvider } from "launchdarkly-react-client-sdk"
+import { basicLogger } from "launchdarkly-js-client-sdk"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const clientSideID = process.env.REACT_APP_LD_CLIENT_ID;
+
+(async () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})();
+
